38fbfa599204b6a684e9d799bdb35bebbed497e7
sabato <niccolo.s04@gmail.com>
2026-06-07 19:04:12 +0200

    gaps fixed

---
+++ js/tailwind.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            slate: {
+              850: '#1e293b',
+              950: '#020617',
+            }
+          },
+          screens: {
+            'xs': '375px',
+          }
+        }
+      }
+    }
